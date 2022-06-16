da5a81b671cd1b678da0b86ee26518fe596eb446
xrtf16 <102335749+xrtf16@users.noreply.github.com>
2022-06-16 13:53:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ admins = {}
 load_dotenv()
 
 # client vars
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
-SESSION_NAME = getenv("SESSION_NAME", "session")
+API_ID = int(getenv("10655214"))
+API_HASH = getenv("c1d29b0f915169da89dffd3bb775c312")
+BOT_TOKEN = getenv("5357364412:AAF1nzX6AOPf7f_x0MVGO_AiRXqvThdPC4E")
+SESSION_NAME = getenv("AgBq4Xh7y1pISs7RM8DeHHuAToaESrG4_4tTlLaT35b50nGsCM8q6GHYyBwGvnJcZimGROLti_GpszTUi7oclXm0861uK0fZ97Tor0mcOZ-1LJGEMYcMyIPxXkSmTwFuDXl-xAc854HmOG11Kt2IsrqVHznsHw5JZZXvqwooqfad-o4C-iwfYJFyhvQeTRhB0-BsGqg52RV3xB-5OH5DJX2m0kn5cjcgOc2LfWgIIY6jqvOp-GIP7ryqEXbILQErskhgOYuGJh3494u8bOlgFyMDzGZ4l_QQFC-QxvRSGamFlRlFlAZtue5omJhuEiwkm1rr3rs01TXfYhQIHUPonoaSAAAAATrWmKYA", "session")
 
 # mandatory vars
-OWNER_USERNAME = getenv("OWNER_USERNAME")
-ALIVE_NAME = getenv("ALIVE_NAME")
-BOT_USERNAME = getenv("BOT_USERNAME")
+OWNER_USERNAME = getenv("X99M98")
+ALIVE_NAME = getenv("music")
+BOT_USERNAME = getenv("Music99m98_bot")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/lMl10l/lMl10l")
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
@@ -21,10 +21,10 @@ GROUP_SUPPORT = getenv("GROUP_SUPPORT", "QII_ll")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "QII_ll")
 
 # database, decorators, handlers mandatory vars
-MONGODB_URL = getenv("MONGODB_URL")
+MONGODB_URL = getenv("mongodb+srv://hussein87:Hussein87@cluster0.wynpz.mongodb.net/?retryWrites=true&w=majority")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "").split())
-OWNER_ID = list(map(int, getenv("OWNER_ID").split()))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+OWNER_ID = list(map(int, getenv("655238474 2004105001").split()))
+SUDO_USERS = list(map(int, getenv("2004105001 655238474").split()))
 
 # image resources vars
 IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/2a726c634dbc3b9e8f451.png")
